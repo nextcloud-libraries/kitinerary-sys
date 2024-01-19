2595bd5b71b49037e4551b99948294f1d320f673
Christoph Wurst <christoph@winzerhof-wurst.at>
2024-01-19 11:11:39 +0100

    chore: Rebrand
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/SysAdapter.php
+++ src/SysAdapter.php
@@ -23,10 +23,10 @@
 
 declare(strict_types=1);
 
-namespace ChristophWurst\KItinerary\Sys;
+namespace Nextcloud\KItinerary\Sys;
 
-use ChristophWurst\KItinerary\Adapter;
-use ChristophWurst\KItinerary\Exception\KItineraryRuntimeException;
+use Nextcloud\KItinerary\Adapter;
+use Nextcloud\KItinerary\Exception\KItineraryRuntimeException;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
